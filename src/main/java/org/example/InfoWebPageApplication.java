8e97340193d451d0077b49fc391740aab05ed4a9
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2022-02-10 10:28:01 +0100

    created Project and added JexxaMain and dependencies

---
+++ src/main/java/org/example/InfoWebPageApplication.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import io.jexxa.core.JexxaMain;
+import io.jexxa.infrastructure.drivingadapter.rest.RESTfulRPCAdapter;
+
+public final class InfoWebPageApplication
+{
+
+    public static void main(String[] args)
+    {
+        //Create your jexxaMain for this application
+        var jexxaMain = new JexxaMain("HelloJexxa");
+
+        jexxaMain
+                // Bind a REST adapter to class HelloJexxa and expose its methods
+                .bind(RESTfulRPCAdapter.class).to(InfoWebPageApplication.class)
+
+                //Start Jexxa and all bindings
+                // - Open following URL in browser to get greetings: http://localhost:7500/HelloJexxa/greetings
+                // - You can also use curl: `curl -X GET http://localhost:7500/HelloJexxa/greetings`
+                .start()
+
+                //Wait until shutdown is called by one of the following options:
+                // - Press CTRL-C
+                // - Use `jconsole` to connect to this application and invoke method shutdown
+                .waitForShutdown()
+
+                //Finally invoke stop() for proper cleanup
+                .stop();
+    }
+}
