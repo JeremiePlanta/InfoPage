f6ec0d08ccb927417f7f5da88f243ba78c377b95
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2022-02-16 08:20:46 +0100

    Changed name to InfoPage

--- src/main/java/org/example/InfoPageApplication.java
+++ src/main/java/org/example/InfoPageApplication.java
@@ -3,17 +3,17 @@ package org.example;
 import io.jexxa.core.JexxaMain;
 import io.jexxa.infrastructure.drivingadapter.rest.RESTfulRPCAdapter;
 
-public final class InfoWebPageApplication
+public final class InfoPageApplication
 {
 
     public static void main(String[] args)
     {
         //Create your jexxaMain for this application
-        var jexxaMain = new JexxaMain("HelloJexxa");
+        var jexxaMain = new JexxaMain("InfoPage");
 
         jexxaMain
                 // Bind a REST adapter to class HelloJexxa and expose its methods
-                .bind(RESTfulRPCAdapter.class).to(InfoWebPageApplication.class)
+                .bind(RESTfulRPCAdapter.class).to(InfoPageApplication.class)
 
                 //Start Jexxa and all bindings
                 // - Open following URL in browser to get greetings: http://localhost:7500/HelloJexxa/greetings
